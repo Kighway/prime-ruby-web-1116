699c7caeb631776a1442fd8155fa7f84a266b674
Kyle Tulod <tulod.ky13@gmail.com>
2016-11-22 16:52:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime? (number)
+  # Add  code here!
+  return false if number <= 1
+  test = 2
+  while test < number
+    return false if number % test == 0
+    test += 1
+  end
+  true
+end
